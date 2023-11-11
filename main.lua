ddb7ba71a632a581e97771b8b95f16af9cc5def7
winterscode <qgv3981g@duck.com>
2023-11-10 19:37:05 -0800

    Base area for starting the program with a file

--- main.lua
+++ main.lua
@@ -1,18 +1,30 @@
 
-io.write("$ ")
-local Lexer = require("libs.lex").new(io.read())
-require("libs.rebind")
-local tokens, err = Lexer:Tokenize()
+local shell = arg[1] == nil
 
-if err then
-    error(err:GenerateStackTrace())
+if shell then
+    while true do
+        io.write("$ ")
+        local line = io.read()
+
+        if line == "q" then break end
+
+        local Lexer = require("libs.lex").new(line)
+        require("libs.rebind")
+        local tokens, err = Lexer:Tokenize()
+
+        if err then
+            error(err:GenerateStackTrace())
+        end
+        local Parser = require("libs.parse").new(tokens)
+        local AST, err = Parser:Parse()
+
+        if err then
+            error(err:GenerateStackTrace())
+        end
+
+        local Interpreter = require("libs.inter").new(AST)
+        Interpreter:Interpret()
+    end
+else
+    print("Ran file " .. arg[1])
 end
-local Parser = require("libs.parse").new(tokens)
-local AST, err = Parser:Parse()
-
-if err then
-    error(err:GenerateStackTrace())
-end
-
-local Interpreter = require("libs.inter").new(AST)
-Interpreter:Interpret()
