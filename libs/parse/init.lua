7689b33434539f2fefe6456857f92537e74f6f9c
winterscode <qgv3981g@duck.com>
2024-01-12 08:51:02 -0800

    Commenting

--- libs/parse/init.lua
+++ libs/parse/init.lua
@@ -11,98 +11,119 @@ local BinOp = require('libs.parse.node.BinOp')
 local UnOp = require('libs.parse.node.UnOp')
 local Number = require('libs.parse.node.Number')
 local ParenScope = require('libs.scope.paren')
+
+--// Get the required global modules to change stuff
 require('libs.consts')
 require('libs.rebind')
 
 --// Create a new parser instance
 function Parser.new(tokens)
+
+    -- Create the parser instance
     local self = setmetatable({}, Parser)
 
+    -- Set all of the data about the parser
     self.tokens = tokens
     self.currentTokenIdx = 0
     self.currentToken = nil
     self.lastToken = nil
-    self.parenScope = ParenScope.new()
 
+    -- Set up positioning
     self:Advance()
 
+    -- Return the parser instance
     return self
 end
 
 --// Movement between the tokens in the parser
 function Parser:Advance()
+    
+    -- Set what the last token was
     self.lastToken = self.currentToken or self.lastToken
+
+    -- Increase the position of the current token
     self.currentTokenIdx = self.currentTokenIdx + 1
     self.currentToken = self.tokens[self.currentTokenIdx] or nil
 end
 
 --// Create a BinOp node
 function Parser:GenerateBinOp(func, operators)
+
+    -- Get the left side of the binary operation
     local left = self[func](self)
 
+    -- Thile the token is one of the operators
     while self.currentToken ~= nil and table.find(operators, self.currentToken.tokenType) do
+
+        -- Get the operator of the binary operation
         local operator = self.currentToken
         self:Advance()
+
+        -- Get the right side of the binary operation
         local right = self[func](self)
+
+        -- Create the binary operation node
         left = BinOp.new(left, operator, right):SetPosition(left.position)
     end
-    if (self.currentToken or self.lastToken).tokenType == TokenType.TT_RPAREN then
-        self.parenScope:SubParen()
-    end
 
+    -- Return the created node
     return left
 end
 
 --//// NAMES ARE BASED ON PEMDAS, THE ORDER OF OPERATIONS
---// Order of operations
+
+--// Factor just returns a number
 function Parser:Factor() 
+
+    -- Get the current token
     local token = self.currentToken
 
+    -- If the current token is a number
     if token and token.tokenType == TokenType.TT_NUMBER then
-        -- self:Advance()
+
+        -- Get and return a number with the value
         local num = Number.new(token):SetPosition(token.position)
         return num
     end
 
+    -- Return an error since it's supposed to have a value
     return Number.new(self.lastToken):SetError(InvalidSyntaxError.new("Missing value in expression", self.lastToken.position))
 end
-function Parser:P()
-    if not self.currentToken then
-        return Number.new(self.lastToken):SetError(InvalidSyntaxError.new("Missing value in expresion", self.lastToken.position))
-    elseif self.currentToken.tokenType == TokenType.TT_LPAREN then
-        self:Advance()
-        self.parenScope:AddParen()
-        return self:AS()
-    end
-    self:Advance()
-    return self:Factor()
-end
+
+--// Exponentiation
 function Parser:E()
-    return self:GenerateBinOp("P", {TokenType.TT_POW})
+
+    -- Return a binary operation for powers
+    return self:GenerateBinOp("Factor", {TokenType.TT_POW})
 end
+
+--// Multiplication and division
 function Parser:MD()
+
+    -- Return a binary operation using multiplication and division
     return self:GenerateBinOp("E", {TokenType.TT_MUL, TokenType.TT_DIV})
 end
+
+--// Addition and subtraction
 function Parser:AS()
+
+    -- Return a binary operation using addition and subtraction
     return self:GenerateBinOp("MD", {TokenType.TT_ADD, TokenType.TT_SUB})
 end
 
 --// Parse the tokens
 function Parser:Parse()
 
+    -- Create the run node out from addition and subtraction
     local res = self:AS()
 
-    if self.parenScope.parentheses > 0 then
-        res:SetError(InvalidSyntaxError.new("Missing ending parenthesis", self.lastToken.position))
-    elseif self.parenScope.parentheses < 0 then
-        res:SetError(InvalidSyntaxError.new("Missing starting parenthesis", self.lastToken.position))
-    end
-
+    -- If there is an error, return the error and no result
     if res.error then
         return nil, res.error
     end
-    return res, nil
 
+    -- Return the result
+    return res, nil
 end
 
 --// Return the module
