aef0594439b4046ae3cc9c39ae47d0d2d2c1b918
winterscode <qgv3981g@duck.com>
2024-02-06 14:16:18 -0800

    Started creation on the new interpreter system

--- libs/inter/init.lua
+++ libs/inter/init.lua
@@ -2,6 +2,8 @@
 local Interpreter = {}
 Interpreter.__index = Interpreter
 
+require('libs.rebind')
+
 function Interpreter.new(ast, globalScope)
     local self = setmetatable({}, Interpreter)
 
@@ -13,8 +15,19 @@ end
 
 function Interpreter:Interpret()
 
-    return self.ast:Operate(self.globalScope)
+    local firstNode = self.ast.node
+    self['Visit_'..typeof(firstNode)](self)
 
+end
+
+function Interpreter:Visit_BinOp()
+    print('Binary operation')
+end
+function Interpreter:Visit_Number()
+    print('Number')
+end
+function Interpreter:Visit_nil()
+    error('No visit method for node type `{typeof(node)}`')
 end
 
 function Interpreter.__type()
